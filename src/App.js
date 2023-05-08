778c0cc33763a194e3b5c9c1dec12d08034a1fc3
vamshikrishna0206 <121841353+vamshikrishna0206@users.noreply.github.com>
2023-05-08 12:50:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import arr from "./data";
+import findTime from "./Component/findTime";
+import Tbody from "./Component/Tbody";
+// import Canvas from "./Component/Canvas";
+let App = () => {
+  let [data, setData] = useState([...arr]);
+  let [amap, setamap] = useState([]);
+  let totalTime = [];
+
+  let obj = {};
+
+  useEffect(() => {
+    for (let i = 0; i < data.length; i++) {
+      if (obj[data[i].EmployeeName]) {
+        obj[data[i].EmployeeName].push(data[i]);
+      } else {
+        obj[data[i].EmployeeName] = [data[i]];
+      }
+    }
+
+    for (let key in obj) {
+      let cdata = obj[key];
+      let ttime = 0;
+
+      for (let i = 0; i < cdata.length; i++) {
+      ttime += findTime(cdata[i]);}
+      ttime /= 60;
+      totalTime.push({ ttime, key });
+      setamap(totalTime)
+    }
+  }, []);
+
+  return (
+    <div>
+      <table border='2'>
+        <tbody> {amap.map((item, index) => <Tbody item={item} ind={index} key={index }/>)} </tbody>
+      </table>
+      {/* <Canvas></Canvas> */}
+
+    </div>
+  );
+};
+
+export default App;
